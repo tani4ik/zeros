870543ea6299151e0a03efd9319664be388673fb
Tatsiana Sakalinskaya <tani4ik@gmail.com>
2018-09-30 21:21:31 +0300

    Counted zeros of factorial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let zeros = 0;
+  while (number > 0) {
+    number = Math.floor(number / 5);
+    zeros += number;
+  }
+  return zeros;
 }
